e67c67b4f73026c27accaa531b8228ac543415b6
Koryi <502554333@qq.com>
2019-05-20 21:14:02 +0800

    Til the Judge
    Judge not work

---
+++ Gobang-UnFinished/game.js
@@ -0,0 +1,25 @@
+var config = {
+  width: 80 * 10,
+  height: 80 * 10,
+  type: Phaser.AUTO,
+  scene: [LoaderScene, BootScene, GameScene],
+  title: 'Phaser 3 Multiplayer Go Bang',
+  pixelArt: true,
+  backgroundColor: 'ffffff'
+};
+
+var game = new Phaser.Game(config);
+
+//resizing window resizes the game
+game.events.on('resize', resize, game);
+
+function resize (width, height)
+{
+    if (width === undefined) { width = game.sys.game.config.width; }
+    if (height === undefined) { height = game.sys.game.config.height; }
+
+    game.cameras.resize(width, height);
+
+    game.bg.setSize(width, height);
+    game.logo.setPosition(width / 2, height / 2);
+}
